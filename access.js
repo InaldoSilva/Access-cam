d3db848ee048f2a1d323e07209ef5bda8c1c6767
Inaldo Silva <104039452+InaldoSilva@users.noreply.github.com>
2024-04-26 14:53:38 -0300

    first commit

---
+++ access.js
@@ -0,0 +1,13 @@
+function accessCamera() {
+
+    const specs = {video:{width:1080, aspectRatio:21/9}}
+
+    navigator.mediaDevices.getUserMedia(specs).then(stream =>{
+
+        const videoElement = document.querySelector("#video")
+        videoElement.srcObject = stream
+        
+    }).catch(error=>{console.log(error)});
+}
+
+window.addEventListener("DOMContentLoaded", accessCamera);
